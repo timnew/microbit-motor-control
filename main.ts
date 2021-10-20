067755b4f16ee13decd597616974d23538bc8a81
TimNew <timnew@users.noreply.github.com>
2021-10-20 14:06:24 +1100

    Simplify the control

--- main.ts
+++ main.ts
@@ -1,28 +1,16 @@
-function switchMode (newMode: boolean) {
-    isContinuesControl = newMode
-    angle = 135
-    if (isContinuesControl) {
-        basic.showIcon(IconNames.Happy)
-    } else {
-        basic.showIcon(IconNames.Asleep)
-    }
-}
-function sustainControlRoutine () {
-    angle += pins.map(
+let motorStates = 0
+radio.setGroup(1)
+radio.setTransmitPower(7)
+basic.showIcon(IconNames.Happy)
+basic.forever(function () {
+    radio.sendValue("angle", pins.map(
     pins.analogReadPin(AnalogPin.P1),
     0,
     1023,
-    -5,
-    5
-    )
-    if (angle < 0) {
-        angle = 0
-    } else if (angle > 270) {
-        angle = 270
-        radio.sendValue("angle", angle)
-    }
-}
-function mortorControlRoutine () {
+    0,
+    270
+    ))
+    motorStates = 0
     if (GHBit.Button(GHBit.enButton.B1, GHBit.enButtonState.Press)) {
         motorStates = motorStates ^ 0x01
     } else if (GHBit.Button(GHBit.enButton.B2, GHBit.enButtonState.Press)) {
@@ -33,31 +21,4 @@ function mortorControlRoutine () {
         motorStates = motorStates ^ 0x08
     }
     radio.sendValue("motor", motorStates)
-}
-function continueControlRoutine () {
-    radio.sendValue("angle", pins.map(
-    pins.analogReadPin(AnalogPin.P1),
-    0,
-    1023,
-    0,
-    270
-    ))
-}
-let angle = 0
-let isContinuesControl = false
-let motorStates = 0
-radio.setGroup(1)
-radio.setTransmitPower(7)
-motorStates = 0
-switchMode(true)
-basic.forever(function () {
-    if (GHBit.Rocker(GHBit.enRocker.Press)) {
-        switchMode(!(isContinuesControl))
-    }
-    if (isContinuesControl) {
-        continueControlRoutine()
-    } else {
-        sustainControlRoutine()
-    }
-    mortorControlRoutine()
 })
